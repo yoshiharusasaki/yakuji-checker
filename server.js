73051b0a7c1f53c8af8801387c16b9f668efb974
yoshiharusasaki <y-sasaki@fine-kagaku.co.jp>
2026-06-23 10:42:34 +0900

    Fix: remove duplicate node-fetch require (use native fetch)

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
 const multer = require('multer');
 const fetch = (...args) => globalThis.fetch(...args);
